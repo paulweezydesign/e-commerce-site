d6c800f0eeda3f25f3c758bfa0455cd1a21d0365
paulweezydesign <weezy@paulweezydesign.dev>
2023-01-27 22:52:03 +0000

    added styles and product cards

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import styles from './Header.module.scss'
+import { FaShoppingCart } from 'react-icons/fa'
 import Container from '@components/Container/Contaner';
 const Header = () => {
   return (
     <header className={styles.header}>
      <Container className={styles.headerContainer}>
-      <p className={styles.headerTitle}>Weezy&apos;s Store</p>
-      <p className='text-white'>$0.00</p>
+      <p className={styles.headerTitle}>Hyper Bros. Trading Cards</p>
+      <p className='text-white flex items-center'>
+      <FaShoppingCart className='mr-4' />
+      $0.00</p>
       </Container>
     </header>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
 import Header from '@components/Header/Header'
 import Footer from '@components/Footer'
-
+import Container from '@components/Container/Contaner'
+import Button from '@components/Button/Button'
+import products from '@data/products.json'
 export default function Home() {
   return (
     <div className="container">
@@ -11,10 +14,24 @@ export default function Home() {
       </Head>
       <Header  />
       <main>
-       
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <Container className=''>
+        <h1 className='text-3xl font-bold'>Hyper Bros. Trading Cards </h1>
+        <h2>Available Cards</h2>
+        <ul className={styles.products}>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+        <img src={product.image} alt={`Card of ${product.title}`} />
+        <h3 className='text-[1.2em] font-bold mt-2 '>{product.title}</h3>
+        <p className='mt-1'>{product.price}</p>
+        <Button className='mt-2'>Add to Cart</Button>
+        </li>
+          )
+        })}
+        
+        
+        </ul>
+        </Container>
       </main>
 
       <Footer />
